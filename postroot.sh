9d7082db6af4a08eae9950150668c7a81a822f5a
svethi <svethi@loxberry.de>
2019-04-04 11:39:53 +0200

    MQTT prerelease

--- postroot.sh
+++ postroot.sh
@@ -9,3 +9,20 @@ then
 	echo "<INFO> unlink /etc/init.d/callmonitor"
 	rm -f "/etc/init.d/callmonitor"
 fi
+
+if [ -f "/etc/systemd/system/callmonitor" ]
+then
+	
+	echo "<INFO> callmonitor stoppen."
+	/bin/systemctl stop callmonitor.service
+	rm -f "/etc/systemd/system/callmonitor"
+	/bin/systemctl daemon-reload
+fi
+
+if [ -f "/etc/rsyslog.d/callmonitor.conf"]
+then
+	
+	echo "<INFO> rsyslog zurücksetzen."
+	rm -f "/etc/rsyslog.d/callmonitor.conf"
+	/bin/systemctl restart rsyslog
+fi 
